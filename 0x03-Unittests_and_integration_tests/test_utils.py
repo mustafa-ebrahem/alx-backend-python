616bfe4efba135768daab68e7756af556ef78c25
Mostafa <mustafa.ebrahem@outlook.com>
2025-06-01 14:13:20 +0300

    Add test cases for exception handling in access_nested_map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,3 +12,11 @@ class TestAccessNestedMap(TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         result = access_nested_map(nested_map=nested_map, path=path)
         self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        result = access_nested_map(nested_map=nested_map, path=path)
+        self.assertRaises(result)
